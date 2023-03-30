373da718481cc6fe2ef91d2336903d930767695c
Elias Achi <elias@plazacybersecurity.com>
2023-03-29 23:45:25 -0500

    Adding relauncher/updater

---
+++ restart_sorullo.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+# This script will restart sorullo and update to its latest version. It assumes you have a valid docker-compose.yml and you're using docker compose as a docker plugin.
+docker compose down && git pull && docker compose build && docker compose up -d && docker compose logs -f
