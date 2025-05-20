ee0c0615284cb355975933525706070023692f70
Elias Achi <wantan@gmail.com>
2025-05-20 15:58:17 -0500

    Fix command for auto launch

--- restart_sorullo.sh
+++ restart_sorullo.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # This script will restart sorullo and update to its latest version. It assumes you have a valid docker-compose.yml and you're using docker compose as a docker plugin.
-docker compose down && git pull && docker compose build && docker compose up -d && docker compose logs -f
+docker compose down && git pull && docker compose build && docker compose up -d
+
